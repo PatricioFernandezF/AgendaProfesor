29859d295e18d2f04da96b973567806d2d236a77
aldevaran <aldevaran@aldevaran-desktop>
2017-03-30 13:01:40 +0200

    Creada la base del fichero usuario para acoplar con el vector de punteros

--- usuario.c
+++ usuario.c
@@ -18,9 +18,14 @@ void alta_usuario(Usuario* usuario)
     int opc=0;
     
     Usuario newUser;
+    char    cadena[50]; 
     
-    printf("Introduce el nombre del usuario");
-    fscanf(stdin, "%s", newUser.nombre);
+    printf("Introduce el nombre del usuario (20 cáracteres):");
+    do{
+        fscanf(stdin, "%s", cadena);
+    }while(strlen(cadena)>20);
+    strcpy(cadena,newUser.nombre);
+    
     printf("Selecciona el tipo de usuario:\n1)Administrador\n2)Profesor\nPerfil:");
     do{
         switch(opc){
@@ -30,26 +35,112 @@ void alta_usuario(Usuario* usuario)
             case 2:
                 strcpy("profesor",newUser.perfil);
                 break;
+            default:
+                printf("\nOpción no valida\n");
         }
     }while(opc!=1 || opc!=2);
+    printf("Introduce un nombre de usuario (5 cáracteres):");
+    do{
+        fscanf(stdin, "%s", cadena);
+    }while(strlen(cadena)>5);
+    strcpy(cadena,newUser.login);
+    
+    printf("Introduce una contraseña (8 cáracteres):");
+    do{
+        fscanf(stdin, "%s", cadena);
+    }while(strlen(cadena)>8);
+    strcpy(cadena,newUser.pass);
+    
+    printf("\n\nDatos intorucidos en la ficha.\Los datos introducidos son:\n");
+    printf("Nombre: %s\n",newUser.nombre);
+    printf("Perfil: %s\n",newUser.perfil);
+    printf("Login %s\n",newUser.login);
+    printf("Pass: %s\n",newUser.pass);
+    
+    //Priceso para añadir al vector el usuario de nueva creación
+    printf("\n\nAñadido a la lista de usuarios.");
+    
+    
 }
 // Cabecera: void baja_usuario(Usuario*)
 // Precondicion: Recibe una cadena de caracteres de un usuario.
 // Poscondicion:
 void baja_usuario(Usuario* usuario)
 {
+    int num;
+    
+    listar_usuario(&usuario);
+    printf("Indica que usuario desea dar de baja:");
+    scanf("%d",&num);
+    
 }
 // Cabecera: void modificar_usuario(Usuario*)
 // Precondicion: Recibe una cadena de caracteres de un usuario.
 // Poscondicion:
 void modificar_usuario(Usuario* usuario)
 {
+    int numUser, numOpc, opc;
+    char cadena[50];
+    
+    listar_usuario(&usuario);
+    printf("Inidica que usuario desea modificar:");
+    scanf("%d",&numUser);
+    printf("Injdica que dato desea modificar:"
+            "\n1)Nombre\n2)Perfil\n3)Login\n4)Pass");
+    scanf("%d",&numOpc);
+    
+    do{
+        switch(numOpc){
+            case 1:
+                printf("Introduce el nombre del usuario (20 cáracteres):");
+                do{
+                    fscanf(stdin, "%s", cadena);
+                }while(strlen(cadena)>20);
+                strcpy(cadena,newUser.nombre);
+                break;
+
+            case 2:
+                printf("Selecciona el tipo de usuario:\n1)Administrador\n2)Profesor\nPerfil:");
+                do{
+                    switch(opc){
+                        case 1:
+                            strcpy("administrador",newUser.perfil);
+                            break;
+                        case 2:
+                            strcpy("profesor",newUser.perfil);
+                            break;
+                    }
+                }while(opc!=1 || opc!=2);
+                printf("Introduce un nombre de usuario (5 cáracteres):");
+                break;
+
+            case 3:
+                do{
+                    fscanf(stdin, "%s", cadena);
+                }while(strlen(cadena)>5);
+                strcpy(cadena,newUser.login);
+                break;
+
+            case 4:
+                printf("Introduce una contraseña (8 cáracteres):");
+                do{
+                    fscanf(stdin, "%s", cadena);
+                }while(strlen(cadena)>8);
+                strcpy(cadena,newUser.pass);
+                break;
+            
+            default:
+                printf("\nOpción no valida\n");
+        }
+    }while(numOpc<1 || numOpc>4);
+    
 }
 // Cabecera: void listar_usuario(Usuario*)
 // Precondicion: Recibe una cadena de caracteres de un usuario.
 // Poscondicion:
 void listar_usuario(Usuario* usuario)
 {
+    
 }
 // Cabecera: void menu_usuario()
 // Precondicion:
@@ -81,6 +172,6 @@ void menu_usuario()
         default:
             printf("Error al elegir la opcion.\t");
             break;
-        }while(x!=0)
-    }
+        }
+    }while(x!=0);
 }
