70f23cc24f816821e9998c3ff43b33611f8fd1e5
jmarrieta98 <jmarrieta98@gmail.com>
2017-04-26 18:43:39 +0200

    Delete alumnos.c

--- alumnos.c
+++
@@ -1,8 +0,0 @@
-#include "alumnos.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "fichero.h"
-
-/*cabecera: void menuCalificaciones()
-precondición: Ser llamado por el usuario.
-postcondición: Mostrar menu.*/
