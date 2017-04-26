4a6693d4d0c060239a046ba91a5e99837333b238
jmarrieta98 <jmarrieta98@gmail.com>
2017-04-26 18:45:49 +0200

    Add files via upload

---
+++ matricula.h
@@ -0,0 +1,10 @@
+#ifndef MATRICULA_H_INCLUDED
+#define MATRICULA_H_INCLUDED
+
+#include "fichero.h"
+void lista_de_materias();
+void cambios_de_matricula();
+void eliminar_matricula();
+void crear_nueva_matricula();
+
+#endif // MATRICULA_H_INCLUDED
